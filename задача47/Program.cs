958e75ba2765b7ef601cf7c686fdc89153d0ae62
dasha.lisikhina@yandex.ru <dasha/lisikhina@yandex.ru>
2022-08-29 20:54:24 +0400

    задача 47

---
+++ задача47/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа задает массив случайных вещественных чисел, размером m*n
+Console.WriteLine("Введите количество строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[, ] array = new double[m, n];
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        array[i, j] = new Random().NextDouble()*100;
+        Console.Write(String.Format("{0,5:f1}", array[i, j] + " "));
+    }
+    Console.WriteLine();
+}
