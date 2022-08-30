2fa6fadeede43688c3c1f326ec941ad2eff99509
dasha.lisikhina@yandex.ru <dasha/lisikhina@yandex.ru>
2022-08-30 13:17:24 +0400

    задача 52

---
+++ задача52/Program.cs
@@ -0,0 +1,45 @@
+﻿// 
+int[, ] array = new int[3, 4];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(0,11);
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+void SA (int[, ] array)
+{
+    double Sum1 = 0;
+    double Sum2 = 0;
+    double Sum3 = 0;
+    double Sum4 = 0;
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+    if (j == 0)
+    {
+        Sum1 = Sum1 + array[i, j];
+    }
+    if (j == 1)
+    {
+        Sum2 = Sum2 + array[i, j];
+    }
+    if (j == 2)
+    {
+        Sum3 = Sum3 + array[i, j];
+    }
+    if (j == 3)
+    {
+        Sum4 = Sum4 + array[i, j];
+    }
+    }
+    
+}
+Console.WriteLine($"{Sum1 / 3}, {Sum2 / 3}, {Sum3 / 3}, {Sum4 / 3}");
+}
+SA(array);
