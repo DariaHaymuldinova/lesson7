ecd05bc9b1aa4febd1560e35b07d46834e2b44bf
dasha.lisikhina@yandex.ru <dasha/lisikhina@yandex.ru>
2022-08-31 12:46:45 +0400

    задача50

---
+++ задача50/Program.cs
@@ -0,0 +1,39 @@
+﻿// 
+int[,] array = new int[3, 4];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(0, 10);
+        Console.Write(String.Format(array[i, j] + " "));
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool Find(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] == number)
+            {
+                Console.WriteLine($"{i}, {j}");
+                continue;
+                return true;
+            }
+
+        }
+    }
+    return false;
+}
+if (Find(array))
+{
+    Console.WriteLine("Такого числа нет");
+}
+else
+{
+    Console.WriteLine("Такого числа нет");
+}
